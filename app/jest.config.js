1d87e8cbfcf734a27305d0219b109371bfc82043
Sawyer <git@sawyerh.com>
2022-09-29 10:44:56 -0700

    Run formatter

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,21 +1,21 @@
 // See https://nextjs.org/docs/testing
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: [
-    '<rootDir>/tests/jest.setup.js',
-    '<rootDir>/tests/jest-i18n.ts',
+    "<rootDir>/tests/jest.setup.js",
+    "<rootDir>/tests/jest-i18n.ts",
   ],
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-}
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+};
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
