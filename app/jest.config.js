a5c6bd23f08d6a41a7d9def76a32661da582ce45
Sawyer <git@sawyerh.com>
2022-09-28 18:41:13 -0700

    Add context for ‘dir’

--- app/jest.config.js
+++ app/jest.config.js
@@ -2,6 +2,7 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
