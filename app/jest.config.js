922439e60e8f52ffef687b59b731e62af29f94ef
Sawyer Hollenshead <git@sawyerh.com>
2022-09-29 10:52:45 -0700

    Simplify Jest setup by using next/jest (#31)
    ## Changes
    
    - Simplify Jest dependencies and config by usings Next.js's built-in
    Jest support.
    
    ## Context for reviewers
    
    https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
    
    > Since the release of [Next.js 12](https://nextjs.org/blog/next-12),
    Next.js now has built-in configuration for Jest.
    
    Under the hood, next/jest is automatically configuring Jest for you,
    including:
    
    - Setting up transform using
    [SWC](https://nextjs.org/docs/advanced-features/compiler)
    - Auto mocking stylesheets (.css, .module.css, and their scss variants)
    and image imports
    - Loading .env (and all variants) into process.env
    - Ignoring node_modules from test resolving and transforms
    - Ignoring .next from test resolving
    - Loading next.config.js for flags that enable SWC transforms
    
    ## Testing
    
    <img width="714" alt="CleanShot 2022-09-28 at 18 34 38@2x"
    src="https://user-images.githubusercontent.com/371943/192918531-c332470a-87bd-481d-b15a-21197e86e83f.png">

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,29 +1,21 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// See https://nextjs.org/docs/testing
+const nextJest = require("next/jest");
 
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js", // sets up routing to mocks style sheet
-    "^@pages(.*)$": "<rootDir>/pages$1", //allows module imports of page components
-  },
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
   setupFilesAfterEnv: [
     "<rootDir>/tests/jest.setup.js",
     "<rootDir>/tests/jest-i18n.ts",
   ],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  }, //transfrom typescript files to common js for jest compiler
-  transformIgnorePatterns: [
-    "/.next/",
-    "/node_modules/",
-    "^.+\\.module\\.(css|sass|scss)$",
-  ],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-  testRegex: "(/tests/.*(test|spec))\\.[jt]sx?$",
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.ts-jest.json",
-    },
-  },
+  testEnvironment: "jsdom",
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ["node_modules", "<rootDir>/"],
 };
+
+module.exports = createJestConfig(customJestConfig);
