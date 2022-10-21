2dc251e12032a00c9acee506bd5decdecb268364
Sawyer Hollenshead <git@sawyerh.com>
2022-10-21 09:24:04 -0700

    Treat Next.js i18n config as source of truth for other tools' i18n options (#59)
    ## Ticket
    
    #22 
    
    ## Changes
    
    - DRY up Storybook and Jest i18n setup by referencing the config file we
    use for Next.js
    
    ## Context for reviewers
    
    I'd like to see if I can refactor a few more things and add additional
    documentation but tried keep this PR focused on reuse of the config
    file.
    
    ## Testing
    
    Ran Next.js and Storybook locally

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,7 +1,14 @@
+import i18nConfig from "../next-i18next.config";
 // Apply global styling to our stories
 import "../styles/styles.scss";
 // Import i18next config.
 import i18n from "./i18next.js";
+
+// Generate the options for the Language menu using the locale codes.
+// Teams can override these labels, but this helps ensure that the language
+// is at least exposed in the list.
+const initialLocales = {};
+i18nConfig.i18n.locales.forEach((locale) => (initialLocales[locale] = locale));
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -15,6 +22,7 @@ export const parameters = {
   i18n,
   locale: "en",
   locales: {
+    ...initialLocales,
     en: "English",
     es: "Español",
   },
