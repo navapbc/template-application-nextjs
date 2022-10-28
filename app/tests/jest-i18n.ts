6599ff623cb2ac84be28b6022a3141490f539873
Sawyer Hollenshead <git@sawyerh.com>
2022-10-27 18:03:51 -0700

    Dynamically set i18n resources for Storybook and tests (#68)
    ## Changes
    
    - Remove the `resources` option from Storybook's i18next initialization.
    We're utilizing the `i18next-http-backend` package for loading locales
    in Storybook via HTTP requests, so manually setting them doesn't appear
    to be required for it to render the strings.
    - Update Jest's i18next initialization to dynamically set `resources`
    based on the file structure. This removes the need for updating the Jest
    setup file every time a new language file is created.
    - Fix the `test-update` script

--- app/tests/jest-i18n.ts
+++ app/tests/jest-i18n.ts
@@ -1,21 +1,52 @@
-import i18n from "i18next";
+/**
+ * @file Setup internationalization for tests so snapshots and queries reference the correct translations
+ */
+import fs from "fs";
+import i18n, { InitOptions } from "i18next";
+import path from "path";
 import { initReactI18next } from "react-i18next";
 
 // @ts-expect-error - Config file has to be .js
 import i18nConfig from "../next-i18next.config";
-import enCommon from "../public/locales/en/common.json";
-import esCommon from "../public/locales/es/common.json";
 
-// Setup internationalization for tests so snapshots and queries reference the correct translations
-i18n.use(initReactI18next).init({
-  ...i18nConfig,
-  resources: {
-    en: {
-      common: enCommon,
-    },
-    es: { common: esCommon },
-  },
-});
+const locales = i18nConfig.i18n.locales;
+
+/**
+ * Load all of the locales into a single object. A server isn't running for our tests,
+ * so we can't load these static assets using HTTP requests like in Storybook.
+ * https://www.i18next.com/how-to/add-or-load-translations
+ */
+export const getResources = () => {
+  const resources: InitOptions["resources"] = {};
+
+  locales.forEach((locale: string) => {
+    resources[locale] = {};
+
+    const namespaces = fs
+      .readdirSync(path.resolve(__dirname, `../public/locales/${locale}`))
+      .map((file) => file.replace(/\.json$/, ""));
+
+    namespaces.forEach((namespace) => {
+      const namespacePath = `../public/locales/${locale}/${namespace}`;
+      // eslint-disable-next-line
+      resources[locale][namespace] = require(namespacePath);
+    });
+
+    return resources;
+  });
+
+  return resources;
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    ...i18nConfig,
+    resources: getResources(),
+  })
+  .catch((err) => {
+    throw err;
+  });
 
 // Export i18n so tests can manually set the language with:
 // i18n.changeLanguage('es')
