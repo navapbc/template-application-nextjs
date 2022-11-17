c9ded9b17cf34cc8728a779e572cecfae3546a8a
Loren Yu <loren@navapbc.com>
2022-11-17 07:35:36 -0800

    Add renovate.json to install script

--- template-only-bin/install-template.sh
+++ template-only-bin/install-template.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 echo "Fetch latest version of template-application-nextjs"
 git clone --single-branch --branch main --depth 1 git@github.com:navapbc/template-application-nextjs.git
 
-echo "Copy files form template-application-nextjs"
+echo "Copy files from template-application-nextjs"
+cd template-application-nextjs
 cp -r \
-  template-application-nextjs/.github \
-  template-application-nextjs/docs \
-  template-application-nextjs/app \
-  template-application-nextjs/docker-compose.yml \
-  .
+  .github \
+  docs \
+  app \
+  docker-compose.yml \
+  renovate.json \
+  ..
+cd ..
 
 echo "Clean up template-application-nextjs folder"
 rm -fr template-application-nextjs
