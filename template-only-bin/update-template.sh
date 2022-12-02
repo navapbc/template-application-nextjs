56d95d50a920b8615d25e521650f0f6070b01f55
Loren Yu <loren@navapbc.com>
2022-12-02 09:26:47 -0800

    Deploy template to platform-test-nextjs on merges (#93)
    * Add template-only-cd which deploys the application template to the
    platform-test-nextjs test project repo after merges
    * Rename install-template.sh to download-and-install-template.sh and
    split out the cp commands into an install-template.sh script
    * Add an update-template.sh script that resets files the project will
    have modified from the template

---
+++ template-only-bin/update-template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This script updates template-infra in your project. Run
+# This script from your project's root directory.
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname $0)
+
+echo "Install template"
+$SCRIPT_DIR/install-template.sh
+
+# Restore project files with project-specific configuration that was defined as part of project setup.
+# This includes the terraform backend configuration blocks and the project-config module
+# Also restore project files that had lines that were commented out in the template, such as Makefile
+# and cd.yml workflow
+# Updates in any of these files need to be manually applied to the projects
+echo "Restore modified project files"
+git checkout HEAD -- \
+  .github/workflows/cd-storybook.yml
