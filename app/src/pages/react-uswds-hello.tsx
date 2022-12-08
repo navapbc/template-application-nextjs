048cd85bdd809c12f71f78048adcc46a448bb70a
Ali Glenesk <ali.glenesk@gmail.com>
2022-12-08 13:46:38 -0800

    set up a few more things

--- app/src/pages/react-uswds-hello.tsx
+++ app/src/pages/react-uswds-hello.tsx
@@ -1,20 +1,31 @@
-import { Alert, Button, Grid, GridContainer } from "@trussworks/react-uswds";
+import { Alert, Button, Grid, GridContainer, Icon } from "@trussworks/react-uswds";
+
+import Link from "next/link";
 import React from "react";
 
 /** Example page showing use of react-uswds components. Can be deleted once you're on a roll. */
 const HelloWorld = () => {
   return (
     <GridContainer>
-      <Grid>
+      <Grid row gap={5}>
         <Alert type="success" headingLevel="h1" heading="Hello world">
           This is an alert
         </Alert>
       </Grid>
-      <Grid>
-        <p>Here is some example text.</p>
+      <Grid row>
+        <Grid col="auto"><Icon.Build size={9}/></Grid>
+        <Grid col="auto">
+          <p>
+            Here is some example text. You can learn more{" "}
+            <Link href="https://trussworks.github.io/react-uswds/">
+              in the docs
+            </Link>
+            .
+          </p>
+        </Grid>
       </Grid>
-      <Grid>
-        <Button type="button">Click me</Button>
+      <Grid row gap={5}>
+        <Button type="button" onClick={() => {console.log("hello yay")}}>Click me</Button>
       </Grid>
     </GridContainer>
   );
