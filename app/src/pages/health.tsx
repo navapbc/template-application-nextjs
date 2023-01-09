7b189c55011ab2358d29900e10bc096290e4ae18
Loren Yu <loren@navapbc.com>
2023-01-09 14:01:30 -0800

    Run prettier

--- app/src/pages/health.tsx
+++ app/src/pages/health.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import React from "react";
 
 const Health: NextPage = () => {
-  return <React.Fragment>healthy</React.Fragment>
-}
+  return <React.Fragment>healthy</React.Fragment>;
+};
 
 export default Health;
