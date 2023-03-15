a36df6b1bf3e245edabb6482269b224e5e3851da
Sawyer <git@sawyerh.com>
2023-03-15 15:59:51 -0700

    Make it easier to configure Next.js’s basePath config

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -4,7 +4,7 @@
  * @see https://storybook.js.org/docs/configurations/default-config/
  */
 // @ts-check
-const nextConfig = require("../next.config");
+const sassOptions = require("../scripts/sassOptions");
 
 const BASE_PATH = process.env.BASE_PATH ?? "";
 
@@ -45,18 +45,6 @@ const config = {
         "style-loader",
         { loader: "css-loader", options: { url: false } }, // this mirrors next.js behavior
         {
-          loader: "string-replace-loader",
-          options: {
-            // Support deploying Storybook to a subdirectory (like GitHub Pages).
-            // This adds the BASE_PATH to the beginning of all relative URLs in the CSS.
-            // It handles relative urls, whether they are quoted or not.
-            search: /url\(("?)\//g,
-            replace(match, p1, offset, string) {
-              return `url(${p1}${BASE_PATH}/`;
-            },
-          },
-        },
-        {
           /**
            * Next.js sets this automatically for us, but we need to manually set it here for Storybook.
            * The main thing this enables is autoprefixer, so any experimental CSS properties work.
@@ -71,7 +59,7 @@ const config = {
         {
           loader: "sass-loader",
           options: {
-            sassOptions: nextConfig.sassOptions,
+            sassOptions: sassOptions(BASE_PATH),
           },
         },
       ],
--- app/next.config.js
+++ app/next.config.js
@@ -1,16 +1,21 @@
-/** @type {import('next').NextConfig} */
-
+// @ts-check
 const { i18n } = require("./next-i18next.config");
+const sassOptions = require("./scripts/sassOptions");
 
+/**
+ * Configure the base path for the app. Useful if you're deploying to a subdirectory (like GitHub Pages).
+ * If this isn't an empty string, you'll need to set the base path anywhere you use relative paths,
+ * like in `<a>`, `<img>`, or `<Image>` tags. Next.js handles this for you automatically in `<Link>` tags.
+ * @see https://nextjs.org/docs/api-reference/next.config.js/basepath
+ */
+const basePath = "/test";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath,
   i18n,
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [
-      "./node_modules/@uswds",
-      "./node_modules/@uswds/uswds/packages",
-    ],
-  },
+  sassOptions: sassOptions(basePath),
 };
 
 module.exports = nextConfig;
