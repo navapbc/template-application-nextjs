97e1c757c8090122021af40e941d50d8e58af8dc
Sawyer <git@sawyerh.com>
2023-03-16 09:17:39 -0700

    Set basePath to undefined to make clearer its using a default

--- app/next.config.js
+++ app/next.config.js
@@ -4,11 +4,12 @@ const sassOptions = require("./scripts/sassOptions");
 
 /**
  * Configure the base path for the app. Useful if you're deploying to a subdirectory (like GitHub Pages).
- * If this isn't an empty string, you'll need to set the base path anywhere you use relative paths,
- * like in `<a>`, `<img>`, or `<Image>` tags. Next.js handles this for you automatically in `<Link>` tags.
+ * If this is defined, you'll need to set the base path anywhere you use relative paths, like in
+ * `<a>`, `<img>`, or `<Image>` tags. Next.js handles this for you automatically in `<Link>` tags.
  * @see https://nextjs.org/docs/api-reference/next.config.js/basepath
+ * @example "/test" results in "localhost:3000/test" as the index page for the app
  */
-const basePath = "";
+const basePath = undefined;
 const appSassOptions = sassOptions(basePath);
 
 /** @type {import('next').NextConfig} */
