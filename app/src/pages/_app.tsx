282ef588e51c7d2d6b72d45f18307b4790019967
Sawyer <git@sawyerh.com>
2023-06-30 18:42:22 -0700

    Format

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import "../styles/styles.scss";
+
 import Layout from "../components/Layout";
+import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
