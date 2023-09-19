4e18d8c03bb6d01ce4736e63e6675cfa91031c68
Sawyer <git@sawyerh.com>
2023-09-19 15:38:23 -0700

    Add context about how the type declaration file was generated

--- app/next-i18next.config.d.ts
+++ app/next-i18next.config.d.ts
@@ -1,6 +1,7 @@
 /**
  * @file You shouldn't have to worry about this file. Mainly here so imports of the config file
  * (in tests or Storybook) have the correct type for the config object.
+ * This file was generated from the .js file, see https://www.typescriptlang.org/docs/handbook/declaration-files/dts-from-js.html
  */
 declare const _exports: import("next-i18next").UserConfig;
 export = _exports;
