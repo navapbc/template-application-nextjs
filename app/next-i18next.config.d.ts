657fa37c5c6918c735ddab00ac25a25fecc11145
Sawyer <git@sawyerh.com>
2023-09-19 15:46:40 -0700

    Use export default for .d.ts file

--- app/next-i18next.config.d.ts
+++ app/next-i18next.config.d.ts
@@ -1,7 +1,6 @@
 /**
  * @file You shouldn't have to worry about this file. Mainly here so imports of the config file
  * (in tests or Storybook) have the correct type for the config object.
- * This file was generated from the .js file, see https://www.typescriptlang.org/docs/handbook/declaration-files/dts-from-js.html
  */
-declare const _exports: import("next-i18next").UserConfig;
-export = _exports;
+declare const UserConfig: import("next-i18next").UserConfig;
+export default UserConfig;
