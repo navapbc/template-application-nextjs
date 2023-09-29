a918628764c07e0d4abe4162fbda8ded6d3cc514
Sawyer <git@sawyerh.com>
2023-09-29 10:51:33 -0700

    Fix Storybook

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,17 +1,9 @@
 // @ts-check
-import i18nConfig from "../next-i18next.config";
-
 // Apply global styling to our stories
 import "../src/styles/styles.scss";
 
 // Import i18next config.
 import i18n from "./i18next.js";
-
-// Generate the options for the Language menu using the locale codes.
-// Teams can override these labels, but this helps ensure that the language
-// is at least exposed in the list.
-const initialLocales = {};
-i18nConfig.i18n.locales.forEach((locale) => (initialLocales[locale] = locale));
 
 const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -49,7 +41,6 @@ const preview = {
   globals: {
     locale: "en",
     locales: {
-      ...initialLocales,
       en: "English",
       es: "Español",
     },
