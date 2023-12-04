8e4ea30adda8442092d8fc666045d9cadf61dcee
Ali Glenesk <ali.glenesk@gmail.com>
2023-12-04 09:16:51 -0800

    add mocks

---
+++ app/__mocks__/@aws-sdk/client-evidently.ts
@@ -0,0 +1,10 @@
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-client-evidently/Interface/EvaluateFeatureCommandOutput/
+export class Evidently {
+    evaluateFeature = () => {
+      return Promise.resolve({
+        details: 'experimentName',
+        reason: 'LAUNCH_RULE_MATCH',
+        value: { boolValue: true },
+      })
+    }
+  }
