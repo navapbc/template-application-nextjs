7a8d8f428796533a2e32ac3297ffa53ca7c715b2
Sawyer <git@sawyerh.com>
2023-12-05 15:56:22 -0800

    Refactor typing

--- app/src/i18n/index.ts
+++ app/src/i18n/index.ts
@@ -11,23 +11,23 @@ type RequestConfig = Awaited<
 export type Messages = RequestConfig["messages"];
 
 /**
+ * List of languages supported by the application. Other tools (Storybook, tests) reference this.
+ * These must be BCP47 language tags: https://en.wikipedia.org/wiki/IETF_language_tag#List_of_common_primary_language_subtags
+ */
+export const locales = ["en-US", "es-US"] as const;
+export type Locale = (typeof locales)[number];
+export const defaultLocale: Locale = "en-US";
+
+/**
  * All messages for the application for each locale.
  * Don't export this object!! Use `getLocaleMessages` instead,
  * which handles fallbacks to the default locale when a locale
  * is missing a translation.
  */
-const _messages: { [locale: string]: Messages } = {
+const _messages: { [locale in Locale]: Messages } = {
   "en-US": enUs,
   "es-US": esUs,
 };
-
-/**
- * List of languages supported by the application. Other tools (Storybook, tests) reference this.
- * These must be BCP47 language tags: https://en.wikipedia.org/wiki/IETF_language_tag#List_of_common_primary_language_subtags
- */
-export const locales = ["en-US", "es-US"];
-export type SupportedLocale = (typeof locales)[number];
-export const defaultLocale: SupportedLocale = "en-US";
 
 /**
  * Define the default formatting for date, time, and numbers.
@@ -46,10 +46,21 @@ export const formats: RequestConfig["formats"] = {
  * translations are missing from the current locale, the missing key will
  * fallback to the default locale
  */
-export function getLocaleMessages(locale: string = defaultLocale): Messages {
-  let messages = _messages[locale];
+export function getLocaleMessages(
+  requestedLocale: string = defaultLocale
+): Messages {
+  if (requestedLocale in _messages === false) {
+    console.error(
+      "Unsupported locale was requested. Falling back to the default locale.",
+      { locale: requestedLocale, defaultLocale }
+    );
+    requestedLocale = defaultLocale;
+  }
 
-  if (locale !== defaultLocale) {
+  const targetLocale = requestedLocale as Locale;
+  let messages = _messages[targetLocale];
+
+  if (targetLocale !== defaultLocale) {
     const fallbackMessages = _messages[defaultLocale];
     messages = merge({}, fallbackMessages, messages);
   }
