354b7ecab76c7cb4bce30b8c126aad6cfacf5a58
Ali <7331495+aligg@users.noreply.github.com>
2023-12-07 14:46:24 -0800

    Update app/src/services/feature-flags/LocalFeatureFlagManager.ts
    Co-authored-by: Sawyer Hollenshead <git@sawyerh.com>

--- app/src/services/feature-flags/LocalFeatureFlagManager.ts
+++ app/src/services/feature-flags/LocalFeatureFlagManager.ts
@@ -1,7 +1,7 @@
 export class LocalFeatureFlagManager {
   async isFeatureEnabled(featureName: string, userId: string) {
     console.log(
-      `Using mock feature flag manager for feature ${featureName}, user ${userId}`
+      "Using mock feature flag manager", { featureName, userId }
     );
     return Promise.resolve(false);
   }
