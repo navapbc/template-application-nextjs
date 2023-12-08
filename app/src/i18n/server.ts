02b0546c72f33652bfb84e21bc26615d03bfe010
Sawyer <git@sawyerh.com>
2023-12-07 17:09:06 -0800

    Use timeZone

--- app/src/i18n/server.ts
+++ app/src/i18n/server.ts
@@ -1,10 +1,10 @@
 import { getRequestConfig } from "next-intl/server";
 
-import { formats } from "./config";
+import { formats, timeZone } from "./config";
 import { getMessagesWithFallbacks } from "./getMessagesWithFallbacks";
 
 /**
- * I18n config for server components.
+ * Make locale messages available to all server components.
  * This method is used behind the scenes by `next-intl/plugin`, which is setup in next.config.js.
  * @see https://next-intl-docs.vercel.app/docs/usage/configuration#nextconfigjs
  */
@@ -12,5 +12,6 @@ export default getRequestConfig(async ({ locale }) => {
   return {
     formats,
     messages: await getMessagesWithFallbacks(locale),
+    timeZone,
   };
 });
