2d73ff6f025f4d17416539f052ff9d81a47e2680
Sawyer <git@sawyerh.com>
2023-12-13 10:16:24 -0800

    Demonstate test of async server component

--- app/tests/app/page.test.tsx
+++ app/tests/app/page.test.tsx
@@ -1,9 +1,23 @@
 import { axe } from "jest-axe";
-import { View } from "src/app/[locale]/page";
+import Controller, { View } from "src/app/[locale]/page";
 import { LocalFeatureFlagManager } from "src/services/feature-flags/LocalFeatureFlagManager";
-import { render, screen } from "tests/react-utils";
+import { cleanup, render, screen } from "tests/react-utils";
 
-describe("Index", () => {
+describe("Index - Controller", () => {
+  it("retrieves feature flags", async () => {
+    const featureFlagSpy = jest
+      .spyOn(LocalFeatureFlagManager.prototype, "isFeatureEnabled")
+      .mockResolvedValue(true);
+
+    const result = await Controller();
+    render(result);
+
+    expect(featureFlagSpy).toHaveBeenCalledWith("foo", "anonymous");
+    expect(screen.getByText(/Flag is enabled/)).toBeInTheDocument();
+  });
+});
+
+describe("Index - View", () => {
   // Demonstration of rendering translated text, and asserting the presence of a dynamic value.
   // You can delete this test for your own project.
   it("renders link to Next.js docs", () => {
@@ -24,11 +38,14 @@ describe("Index", () => {
   });
 
   it("conditionally displays content based on feature flag values", () => {
-    jest
-      .spyOn(LocalFeatureFlagManager.prototype, "isFeatureEnabled")
-      .mockResolvedValue(true);
+    const enabledFlagTextMatcher = /Flag is enabled/;
 
-    const { container } = render(<View isFooEnabled />);
-    expect(container).toHaveTextContent("Flag is enabled");
+    render(<View isFooEnabled />);
+    expect(screen.getByText(enabledFlagTextMatcher)).toBeInTheDocument();
+
+    cleanup();
+
+    render(<View isFooEnabled={false} />);
+    expect(screen.queryByText(enabledFlagTextMatcher)).not.toBeInTheDocument();
   });
 });
