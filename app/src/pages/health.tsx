c9e912bb5f3e3fbf9184836a78427e71d0a81d95
Sawyer <git@sawyerh.com>
2023-12-12 17:06:44 -0800

    Fix i18n on health page

--- app/src/pages/health.tsx
+++ app/src/pages/health.tsx
@@ -1,4 +1,15 @@
+import { GetServerSideProps } from "next";
+import { getMessagesWithFallbacks } from "src/i18n/getMessagesWithFallbacks";
+
 import React from "react";
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      messages: await getMessagesWithFallbacks(locale),
+    },
+  };
+};
 
 const Health = () => {
   return <>healthy</>;
