9bc7b36e40b0074ff831533390810a42a7925b62
Sawyer <git@sawyerh.com>
2023-12-15 14:26:30 -0800

    Part 1

--- template-only-bin/update-template.sh
+++ template-only-bin/update-template.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-#
-# This script updates template-infra in your project. Run
+# -----------------------------------------------------------------------------
+# This script updates template-application-nextjs in your project. Run
 # This script from your project's root directory.
+#
+# Positional parameters:
+#   TARGET_VERSION (optional) – the version of template-application-nextjs to upgrade to.
+#     Defaults to main.
+# -----------------------------------------------------------------------------
 set -euo pipefail
 
-SCRIPT_DIR=$(dirname $0)
+TARGET_VERSION=${1:-"main"}
+
+echo "Clone template-application-nextjs"
+git clone git@github.com:navapbc/template-application-nextjs.git
+
+# Switch to target version
+cd template-application-nextjs
+git checkout $TARGET_VERSION
+cd -
 
 echo "Install template"
-$SCRIPT_DIR/install-template.sh
+./template-application-nextjs/template-only-bin/install-template.sh
 
 # Restore project files with project-specific configuration that was defined as part of project setup.
-# This includes the terraform backend configuration blocks and the project-config module
-# Also restore project files that had lines that were commented out in the template, such as Makefile
-# and cd.yml workflow
+# Also restore project files that had lines that were commented out in the template.
 # Updates in any of these files need to be manually applied to the projects
 echo "Restore modified project files"
 git checkout HEAD -- \
   .github/workflows/cd-storybook.yml
+
+# Store template version in a file
+cd template-application-nextjs
+git rev-parse HEAD >../.template-nextjs-version
+cd -
+
+echo "Clean up template-application-nextjs folder"
+rm -fr template-application-nextjs
