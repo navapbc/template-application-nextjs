2a1802fb1cbce7958676230333731ee3f8e98eb0
JC Quirin <2581274+jcq@users.noreply.github.com>
2024-01-09 20:31:28 -0500

    Added basic support for local feature flagging - uses env variables - supports hot reload

--- app/src/services/feature-flags/LocalFeatureFlagManager.ts
+++ app/src/services/feature-flags/LocalFeatureFlagManager.ts
@@ -1,6 +1,22 @@
 export class LocalFeatureFlagManager {
   async isFeatureEnabled(featureName: string, userId: string) {
-    console.log("Using mock feature flag manager", { featureName, userId });
-    return Promise.resolve(false);
+    // Note: feature flag env variables should be prefixed with "FEATURE_" or "NEXT_PUBLIC_FEATURE_"
+    // "FEATURE_" is used for server-side feature flags
+    // "NEXT_PUBLIC_FEATURE_" is used for client-side feature flags
+    // Having env var either unset or "false" will disable the feature flag
+    const isEnabled =
+      (process.env[`FEATURE_${featureName}`] &&
+        process.env[`FEATURE_${featureName}`]?.toLowerCase() !== "false") ||
+      (process.env[`NEXT_PUBLIC_FEATURE_${featureName}`] &&
+        process.env[`NEXT_PUBLIC_FEATURE_${featureName}`]?.toLowerCase() !==
+          "false");
+
+    console.log("Using mock feature flag manager", {
+      featureName,
+      userId,
+      isEnabled,
+    });
+
+    return Promise.resolve(isEnabled);
   }
 }
