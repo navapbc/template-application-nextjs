b139c080efb5d8cc51463c4c4df9b5c6a41ab285
Ali Glenesk <ali.glenesk@gmail.com>
2024-12-17 10:05:47 -0800

    clean up

--- app/tests/jest.setup.ts
+++ app/tests/jest.setup.ts
@@ -3,6 +3,3 @@ import "@testing-library/jest-dom";
 import { toHaveNoViolations } from "jest-axe";
 
 expect.extend(toHaveNoViolations);
-
-// Use the mock feature flag adapter for all tests
-process.env.FEATURE_FLAGS_PROJECT = "";
